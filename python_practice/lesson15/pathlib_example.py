30efd0b85f5b5d53c6f7929ab0971e5b177b9054
Oleksandr <alexandrromanchyk@gmail.com>
2026-07-26 16:24:49 +0200

    Added homework15

--- python_practice/lesson15/pathlib_example.py
+++ python_practice/lesson15/pathlib_example.py
@@ -2,59 +2,61 @@ import pathlib
 from constants import BASE_PROJECT_PATH
 import os
 
-current_dir = pathlib.Path().absolute()
-root_dir = pathlib.Path().absolute().parent.parent
+base_path = pathlib.Path(BASE_PROJECT_PATH)
 
-# print(type(current_dir))
-# print(current_dir)
-# print(current_dir.name)
-# print(current_dir.parent)
+current_dir = pathlib.Path().absolute()
+print(type(current_dir))  # <class 'pathlib.PosixPath'>
+print(
+    current_dir)  # поевртає абсолютний шлях до фолдеру "/Users/demianvyrozub/PycharmProjects/hillel2026/python_practice/automation_course_2026/lesson15"
+print(current_dir.name)  # lesson15
+print(current_dir.parent)  # /Users/demianvyrozub/PycharmProjects/hillel2026/python_practice/automation_course_2026
 
 parents = current_dir.parents
 
-# for par in parents:
-#     print(par.name)
+# Виводить багатьох паретнів: automation_course_2026 python_practice hillel2026 PycharmProjects demianvyrozub Users
+for par in current_dir.parents:
+    print(par.name)
 
-# for path_ in current_dir.iterdir():
-#     if path_.is_file():
-#         print(path_.name)
-# print("-"*80)
-# for path_ in current_dir.iterdir():
-#     if path_.is_dir():
-#         print(path_.name)
+# Повертає файли всередині папки якої ми знаходимось
+for path_ in current_dir.iterdir():
+    if path_.is_file:
+        print(path_.name)
 
-# for path_ in root_dir.iterdir():
-#     if path_.is_file():
-#         print(path_.name)
-# print("-"*80)
-# for path_ in root_dir.iterdir():
-#     if path_.is_dir():
-#         print(path_.name)
+    # Перевірка чи це директорія
+for path_ in current_dir.iterdir():
+    if path_.is_dir:
+        print(path_.name)
 
+for path_ in base_path.iterdir():
+    if path_.is_file:
+        print(path_.name)
 
+    # Перевірка чи це директорія
+for path_ in base_path.iterdir():
+    if path_.is_dir:
+        print(path_.name)
 
-# lesson4_full_path = os.path.join(str(current_dir.parent), "lesson4")
-# print(lesson4_full_path)
-#
-# for path_ in pathlib.Path(lesson4_full_path).iterdir():
-#     if path_.is_file():
-#         print(path_.name)
-# print("-"*80)
-# for path_ in pathlib.Path(lesson4_full_path).iterdir():
-#     if path_.is_dir():
-#         print(path_.name)
+# Шукаємо файл
+lesson4_full_path = os.path.join(BASE_PROJECT_PATH, "lesson4")
+print(lesson4_full_path)
 
+# Full path до файлу
+lesson4_full_path = os.path.join(str(current_dir.parent), "lesson4")
+print(lesson4_full_path)
+
+# Всередині lesson4
+for path_ in pathlib.Path(lesson4_full_path).iterdir():
+    if path_.is_file:
+        print(path_.name)
+
+    # Перевірка чи це директорія у середині вже lesson4
+for path_ in pathlib.Path(lesson4_full_path).iterdir():
+    if path_.is_dir:
+        print(path_.name)
+
+# Метод який дозволяє пройтись по всім елементам проекту в корневій папці: повертає список туплів
 file_to_find = "join_example.py"
 
 for current_path, folders, files in os.walk(BASE_PROJECT_PATH):
     if file_to_find in files:
-        print(os.path.join(current_path,file_to_find))
-
-print("-"*80)
-
-partial_file_name = "string"
-
-for current_path, folders, files in os.walk(BASE_PROJECT_PATH):
-    for file in files:
-        if partial_file_name in file:
-            print(os.path.join(current_path,file))
+        print(os.path.join(current_path, file_to_find))
