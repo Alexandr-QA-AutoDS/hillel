0e11589406059cfbb87cb4acf9ab0def650f14d3
Oleksandr <alexandrromanchyk@gmail.com>
2026-08-27 18:48:59 +0200

    Added lesson25

--- tests/functions_tests/factorial_test/factorial_negative_test.py
+++ tests/functions_tests/factorial_test/factorial_negative_test.py
@@ -1,22 +1,35 @@
 import unittest
 import sys
-import pathlib
-sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.parent))
+import logging.config
+import os
+
+import pytest
+
+from constants import BASE_PROJECT_PATH
+
+config_file_path = os.path.join(BASE_PROJECT_PATH, "logging_config.ini") # BASE_PROJECT_PATH + logging_config.ini with correct /
+logging.config.fileConfig(config_file_path) #звідси буде братись файл конфігурації
+sys.path.insert(0, BASE_PROJECT_PATH)
 
 from python_practice.lesson12.function import factorial
+logger = logging.getLogger("root")
 
 class FactorialNegativeTests(unittest.TestCase):
 
+    @pytest.mark.negative
     def test_factorial_negative_number(self):
         expected_error_message = "You have to use 0 or positive numbers. You put -5"
+        logger.info(f"We expect to get ValueError in this test with message {expected_error_message}")
         with self.assertRaises(ValueError) as value_error:
             factorial(-5)
         exception = value_error.exception
         actual_error_message = exception.args[0]
         self.assertEqual(expected_error_message, actual_error_message, msg="Wrong error appeared!!")
 
+    @pytest.mark.negative
     def test_factorial_not_number(self):
         with self.assertRaises(TypeError):
+            logger.info(f"We expect to get TypeError in this test")
             factorial("Hello")
 
 if __name__ == "__main__":
